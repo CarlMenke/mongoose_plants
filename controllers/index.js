b7b8e38aa250f493516f61a132874ac1ea34b801
carlmenke <carlmenke13@gmail.com>
2022-09-07 16:07:10 -0700

    added update and delete routes and conterollers

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,66 @@ const createPlant = async (req, res) => {
     }
 }
 
+const getAllPlants = async (req, res) =>{
+    try {
+        const plants = await Plant.find()
+
+        return res.status(200).json({plants})
+    }
+    catch(error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const getPlantById = async (req,res) =>{
+    try{
+        const plant = await Plant.find({_id: req.params.id})
+        res.status(200).json(plant)
+    }catch(error){
+        return res.status(500).send(error.message)
+    }
+}
+
+const updatePlant = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Plant.findByIdAndUpdate(id, req.body, { new: true }, (err, plant) => {
+            if (err){
+                res.status(500).send(err);
+            }
+            if (!plant) {
+                res.status(500).send('Plant not found!');
+            }
+            return res.status(200).json(plant);
+        })
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+//////////////
+
+const deletePlant = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await Plant.findByIdAndDelete(id)
+        if (deleted) {
+            return res.status(200).send("Plant deleted");
+        }
+        throw new Error("Plant not found");
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+
 module.exports = {
     createPlant,
+    getAllPlants,
+    getPlantById,
+    deletePlant,
+    updatePlant
 }
+
+
+//test plant id :63191c4bf76dc7a81adfa754
